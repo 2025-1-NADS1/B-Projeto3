0ff5120ba95d754b3e09b47891cefbdd3387d75d
Pedro-Picolo <pedrohenriquearaujodelpicolo@gmail.com>
2025-04-18 17:19:52 -0300

    Add files via upload
    pagina de login

---
+++ login.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Pi
+{
+    partial class login
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            pictureBox1 = new PictureBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(220, 227);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "User";
+            textBox1.Size = new Size(365, 23);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.flexcontrol;
+            pictureBox1.Location = new Point(220, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(365, 166);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(220, 273);
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "Password";
+            textBox2.Size = new Size(365, 23);
+            textBox2.TabIndex = 2;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(303, 345);
+            button1.Name = "button1";
+            button1.Size = new Size(184, 26);
+            button1.TabIndex = 3;
+            button1.Text = "Login";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private PictureBox pictureBox1;
+        private TextBox textBox2;
+        private Button button1;
+    }
+}
---
+++ login.cs
@@ -0,0 +1,39 @@
+namespace Pi
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String usuario = textBox1.Text; //pega o texto do textBox1
+            String senha = textBox2.Text;   //pega o texto do textBox2
+            if (usuario == "admin" && senha == "1234") //atribui valores as variaveis e quando correto o valor da acesso a próxima pagina
+            {
+                MessageBox.Show("Login bem-sucedido!"); //mostra login bem-sucedido
+            }
+            else
+            {
+                MessageBox.Show("Usuário ou senha incorretos.");
+            }
+        }
+    }
+}
